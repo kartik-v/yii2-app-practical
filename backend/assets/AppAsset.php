1de5fad982843f8979e2768fa52ad5a9411f084a
duncan-f <f0uad.f@hotmail.com>
2014-05-03 12:47:09 +0400

    Removed the "web" folder for the backend

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -15,8 +15,8 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $basePath = '@backend';
+    public $baseUrl = '@web/backend/assets';
     public $css = ['css/site.css'];
     public $js = [];
     public $depends = [
